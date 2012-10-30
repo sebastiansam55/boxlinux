8828e1c51235e619ee8e54b8289e65a84e2b6792
Sam Sebastian <sebastiansam55@gmail.com>
2012-10-30 00:44:57 -0400

    Wrote file for goo.gl anonymous shortURL service

---
+++ googlshortener.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import requests
+import json
+
+
+def shortenUrl(longURL):
+	url = "https://www.googleapis.com/urlshortener/v1/url"
+	headers = {'Content-type': 'application/json'}
+	data = {'longUrl': longURL}
+	r = requests.post(url=url, headers=headers, data=json.dumps(data))
+	return json.loads(r.content)['id']
